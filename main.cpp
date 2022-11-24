7c0aa2927e9f0a9d6ac72f2bba3cf852c1885673
simonecampi <118023444+simonecampi@users.noreply.github.com>
2022-11-24 19:08:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-int n=0, a=1, b=1, c=0;
-cin >> n;
-if (n>=2) {
-   cout << a << endl << b << endl;
-   for ( int i=2; i < n; i++){
-      c=a+b;
-      a=b;
-      c=c;
-      cout << c <<endl;
+   int n=0, a=1, b=1, c=1;
+   cin>>n;
+   if (n>=2) {
+      cout << a << endl << b << endl;
+      for (int i = 2; i < n; i++) {
+         c=a+b;
+         a=b;
+         b=c;
+         cout << c <<endl;
+      }
    }
-}
-else {
-   cout<<"errore" << endl;
-}
+   else {
+      cout << "errore" <<endl;
+   }
    return 0;
 }
